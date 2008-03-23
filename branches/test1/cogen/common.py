7639d5d7c67e7681e9297d62d07ea46daea42732
ionel.mc <ionel.mc@d0ff8294-6d38-0410-942e-a1d38d1f9975>
2008-03-23 13:50:58 +0000

    made a copy

---
+++ branches/test1/cogen/common.py
@@ -0,0 +1,9 @@
+"""
+A module for quick importing the essential core stuff. 
+(coroutine, Scheduler, events, sockets, priority)
+"""
+from cogen.core.coroutine import coroutine
+from cogen.core.schedulers import Scheduler
+from cogen.core import events
+from cogen.core import sockets
+from cogen.core.events import priority
