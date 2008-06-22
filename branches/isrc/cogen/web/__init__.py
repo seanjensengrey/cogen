1c4c0c7bd5db9a386977b5808bbeb86ac2a53363
ionel.mc <ionel.mc@d0ff8294-6d38-0410-942e-a1d38d1f9975>
2008-06-22 22:18:56 +0000

    removed this useless branch

--- branches/isrc/cogen/web/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-This module holds a wsgi server and future web-related code.
-"""
-import wsgi
--- branches/isrc/examples/bigfile.py
+++
@@ -1,5 +0,0 @@
-import os
-val = os.urandom(1024*1024)
-f = file('BIGFILE', 'wb')
-for i in xrange(100):
-    f.write(val)
