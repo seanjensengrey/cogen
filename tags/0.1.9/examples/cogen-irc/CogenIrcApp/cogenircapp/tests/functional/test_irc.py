21aec26d573f5de65ffda8afb47b7d9b787db206
ionel.mc <ionel.mc@d0ff8294-6d38-0410-942e-a1d38d1f9975>
2008-09-11 13:52:45 +0000

    wops

--- tags/0.1.9/examples/cogen-irc/CogenIrcApp/cogenircapp/tests/functional/test_irc.py
+++
@@ -1,7 +0,0 @@
-from cogenircapp.tests import *
-
-class TestIrcController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url_for(controller='irc'))
-        # Test response...
--- tags/0.1.9/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-To run the unittests get the source package and run setup.py test
-
-
-TODO: test for timeout: operator runs before the timeout but it remains allocated.
-
-"""
-__doc_all__ = []
